fa54b00e090701929d2067120d3b40cf06903044
ncpalmie <palmieronathan@gmail.com>
2021-08-30 14:57:13 -0400

    Removing logger before merging into main

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
-import loggerMiddleware from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 
 import user from "./user";
@@ -27,7 +26,4 @@ const rootReducer = (state, action) => {
   return appReducer(state, action);
 };
 
-export default createStore(
-  rootReducer,
-  applyMiddleware(thunkMiddleware, loggerMiddleware)
-);
+export default createStore(rootReducer, applyMiddleware(thunkMiddleware));
