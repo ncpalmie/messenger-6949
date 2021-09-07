fb902936926d787ae469ab56f4a738632d603297
ncpalmie <palmieronathan@gmail.com>
2021-09-06 22:11:36 -0400

    Combined style sheets between Login and Signup due to similarities

--- client/src/components/Landing/Login.js
+++ client/src/components/Landing/Login.js
@@ -10,99 +10,8 @@ import {
   TextField,
   InputAdornment,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { useStyles } from "./styles";
 import { login } from "../../store/utils/thunkCreators";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flex: "2",
-    height: "fit-content",
-  },
-  header: {
-    display: "flex",
-    width: "100%",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    margin: 50,
-    [theme.breakpoints.down("sm")]: {
-      justifyContent: "center",
-      margin: 10,
-    },
-  },
-  form: {
-    display: "flex",
-    width: "100%",
-    flexDirection: "column",
-    marginLeft: 160,
-    marginTop: 80,
-    [theme.breakpoints.down("sm")]: {
-      justifyContent: "center",
-      alignItems: "center",
-      margin: 0,
-    },
-  },
-  registerButton: {
-    padding: "20px 50px 20px 50px",
-    color: "#3A8DFF",
-    boxShadow: "0px 0px 12px 1px #DCDCDC",
-    fontSize: 20,
-    [theme.breakpoints.down("sm")]: {
-      padding: "10px 25px 10px 25px",
-      fontSize: 15,
-    },
-  },
-  registerText: {
-    marginRight: 35,
-    fontSize: 20,
-    color: "#C8C8C8",
-    textAlign: "right",
-    [theme.breakpoints.down("sm")]: {
-      marginRight: 20,
-      fontSize: 16,
-    },
-  },
-  inputLabel: {
-    marginTop: 20,
-    marginBottom: 20,
-    fontSize: 20,
-    color: "#C8C8C8",
-    [theme.breakpoints.down("sm")]: {
-      margin: 0,
-    },
-  },
-  welcomeText: {
-    fontSize: 35,
-    fontWeight: "bold",
-    marginLeft: -5,
-  },
-  inputFieldContainer: {
-    width: "70%",
-    [theme.breakpoints.down("sm")]: {
-      width: "100%",
-    },
-  },
-  forgotText: {
-    color: "#3A8DFF",
-    fontWeight: "bold",
-  },
-  loginContainer: {
-    display: "flex",
-    justifyContent: "center",
-    width: "70%",
-    marginTop: 80,
-    [theme.breakpoints.down("sm")]: {
-      marginTop: 10,
-      marginLeft: 40,
-    },
-  },
-  loginButton: {
-    backgroundColor: "#3A8DFF",
-    color: "#FFFFFF",
-    fontFamily: "Monserrat, sans-serif",
-    fontSize: 20,
-    padding: "20px 80px 20px 80px",
-  },
-}));
 
 const Login = (props) => {
   const classes = useStyles();
@@ -124,7 +33,7 @@ const Login = (props) => {
   return (
     <Grid container className={classes.root}>
       <Box className={classes.header}>
-        <Typography className={classes.registerText}>
+        <Typography className={classes.switchText}>
           Don't have an account?
         </Typography>
         <Button
@@ -171,12 +80,12 @@ const Login = (props) => {
                 }}
               />
             </FormControl>
-            <Box className={classes.loginContainer}>
+            <Box className={classes.formContainer}>
               <Button
                 type="submit"
                 variant="contained"
                 size="large"
-                className={classes.loginButton}
+                className={classes.formButton}
               >
                 Login
               </Button>
--- client/src/components/Landing/Signup.js
+++ client/src/components/Landing/Signup.js
@@ -9,95 +9,8 @@ import {
   FormControl,
   TextField,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { useStyles } from "./styles";
 import { register } from "../../store/utils/thunkCreators";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flex: "2",
-    height: "fit-content",
-  },
-  header: {
-    display: "flex",
-    width: "100%",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    margin: 50,
-    [theme.breakpoints.down("sm")]: {
-      justifyContent: "center",
-      margin: 10,
-    },
-  },
-  form: {
-    display: "flex",
-    width: "100%",
-    flexDirection: "column",
-    marginLeft: 160,
-    marginTop: 80,
-    [theme.breakpoints.down("sm")]: {
-      justifyContent: "center",
-      alignItems: "center",
-      margin: 0,
-    },
-  },
-  loginButton: {
-    padding: "20px 60px 20px 60px",
-    color: "#3A8DFF",
-    boxShadow: "0px 0px 12px 1px #DCDCDC",
-    fontSize: 20,
-    [theme.breakpoints.down("sm")]: {
-      padding: "10px 25px 10px 25px",
-      fontSize: 15,
-    },
-  },
-  loginText: {
-    marginRight: 35,
-    fontSize: 20,
-    color: "#C8C8C8",
-    textAlign: "right",
-    [theme.breakpoints.down("sm")]: {
-      marginRight: 20,
-      fontSize: 16,
-    },
-  },
-  inputLabel: {
-    marginTop: 20,
-    marginBottom: 20,
-    fontSize: 20,
-    color: "#C8C8C8",
-    [theme.breakpoints.down("sm")]: {
-      marginBottom: 0,
-    },
-  },
-  welcomeText: {
-    fontSize: 35,
-    fontWeight: "bold",
-    marginLeft: -5,
-  },
-  inputFieldContainer: {
-    width: "70%",
-    [theme.breakpoints.down("sm")]: {
-      width: "100%",
-    },
-  },
-  registerContainer: {
-    display: "flex",
-    justifyContent: "center",
-    width: "70%",
-    marginTop: 80,
-    [theme.breakpoints.down("sm")]: {
-      marginTop: 10,
-      marginLeft: 40,
-    },
-  },
-  registerButton: {
-    backgroundColor: "#3A8DFF",
-    color: "#FFFFFF",
-    fontFamily: "Monserrat, sans-serif",
-    fontSize: 20,
-    padding: "20px 80px 20px 80px",
-  },
-}));
 
 const Signup = (props) => {
   const history = useHistory();
@@ -120,7 +33,7 @@ const Signup = (props) => {
   return (
     <Grid container className={classes.root}>
       <Box className={classes.header}>
-        <Typography className={classes.loginText}>
+        <Typography className={classes.switchText}>
           Already have an account?
         </Typography>
         <Button
@@ -172,12 +85,12 @@ const Signup = (props) => {
                 />
               </FormControl>
             </Grid>
-            <Box className={classes.registerContainer}>
+            <Box className={classes.formContainer}>
               <Button
                 type="submit"
                 variant="contained"
                 size="large"
-                className={classes.registerButton}
+                className={classes.formButton}
               >
                 Create
               </Button>
